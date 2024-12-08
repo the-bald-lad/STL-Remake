9781bd2fd5bf3768ee0129b8e97a332c303ed9c5
the-bald-lad <samjbloomfield@btinternet.com>
2024-12-08 16:34:09 +0000

    Started demonstration code

--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -8,19 +8,40 @@
 
 #include "pointers.h"
 
+#include "array.h"
+#include "vector.h"
+
 int main()
 {
-    stl::SharedPointer<int> other {};
+    // Create custom vector
+    stl::Vector<int> vector_of_ints;
 
+    // Add items
+    vector_of_ints.push_back(1);
+    vector_of_ints.push_back(2);
+    vector_of_ints.push_back(3);
+
+    std::cout << "Output vector contents\n";
+
+    // Loop through items using iterator
+    for (const auto& item : vector_of_ints)
+        std::cout << item << " ";
+    std::cout << std::endl;
+
+    // Create custom array
+    stl::Array<stl::UniquePointer<int>, 3> array_of_unique_pointers;
+
+    // Add items
+    array_of_unique_pointers[0] = make_unique<int>(100);
+    array_of_unique_pointers[1] = make_unique<int>(200);
+    array_of_unique_pointers[2] = make_unique<int>(300);
+
+    std::cout << "Output array contents\n";
+
+    // Loop through using iterator, but using lower level syntax
+    for (auto & array_of_unique_pointer : array_of_unique_pointers)
     {
-        stl::SharedPointer<int> ptr = make_shared<int>(5);
-
-        other = ptr;
-
-        std::cout << *ptr << std::endl;
+        std::cout << *array_of_unique_pointer << " ";
     }
 
-    std::cout << *other << std::endl;
-
-    std::cout << "beans\n";
 }
