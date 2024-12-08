2ff99b7567a88f609531fcc5c83a3d5555d6e14c
the-bald-lad <samjbloomfield@btinternet.com>
2024-12-08 17:12:07 +0000

    Tied up file

--- src/sandbox.cpp
+++ src/sandbox.cpp
@@ -4,14 +4,16 @@
 
 // TODO: map
 // TODO: unordered_map
-// TODO: shared/weak pointers
+// TODO: weak pointers
 
 #include "pointers.h"
 
 #include "array.h"
 #include "vector.h"
 
-int main()
+#include "mystring.h"
+
+void exampleOne()
 {
     // Create custom vector
     stl::Vector<int> vector_of_ints;
@@ -27,7 +29,10 @@ int main()
     for (const auto& item : vector_of_ints)
         std::cout << item << " ";
     std::cout << std::endl;
+}
 
+void exampleTwo()
+{
     // Create custom array
     stl::Array<stl::UniquePointer<int>, 3> array_of_unique_pointers;
 
@@ -40,8 +45,28 @@ int main()
 
     // Loop through using iterator, but using lower level syntax
     for (auto & array_of_unique_pointer : array_of_unique_pointers)
-    {
         std::cout << *array_of_unique_pointer << " ";
-    }
+    std::cout << std::endl;
+}
 
+void exampleThree()
+{
+    // Create array of custom strings
+    stl::Array<stl::String, 4> names = { "Sam", "John", "Lisa", "Aimee" };
+
+    const stl::String temp = names[0];
+    names[0] = names[1];
+    names[1] = temp;
+
+    for (const auto& name : names)
+        std::cout << "Your name is: " << name << std::endl;
+}
+
+int main()
+{
+    exampleOne();
+
+    exampleTwo();
+
+    exampleThree();
 }
