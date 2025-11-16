08a28a22b4547e5f14eb0590d37e638725c9b49e
the-bald-lad <samjbloomfield@btinternet.com>
2025-11-16 18:30:06 +0000

    Updated decay to follow expected pattern

--- src/type_traits.h
+++ src/type_traits.h
@@ -4,7 +4,41 @@
 
 #pragma once
 
+#include <type_traits>
+
 namespace traits {
+
+    template <typename T_> constexpr bool is_const = false;
+    template <typename T_> constexpr bool is_const<const T_> = true;
+
+    template <typename T_> constexpr bool is_reference = false;
+    template <typename T_> constexpr bool is_reference<T_&> = true;
+    template <typename T_> constexpr bool is_reference<T_&&> = true;
+
+    template <class> constexpr bool is_array = false;
+    template <class T_, sizet TSize> constexpr bool is_array<T_[TSize]> = true;
+    template <class T_> constexpr bool is_array<T_[]> = true;
+
+    template <typename T_> constexpr bool is_lvalue = false;
+    template <typename T_> constexpr bool is_lvalue<T_&> = true;
+    template <typename T_> constexpr bool is_lvalue<T_&&> = false;
+
+    template <typename T_>
+    constexpr bool is_function = !is_const<const T_> && !is_reference<T_>;
+
+    template <bool>
+    struct select
+    {
+        template <typename T_, typename>
+        using _Apply = T_;
+    };
+
+    template <>
+    struct select<false>
+    {
+        template <typename, typename T_>
+        using _Apply = T_;
+    };
 
     template <typename T_>
     struct remove_ref
@@ -42,21 +76,38 @@ namespace traits {
     using remove_cv_t = remove_cv<T_>::type;
 
     template <typename T_>
-    struct decay
-    { using type = remove_cv<typename remove_ref<T_>::type>::type; };
+    struct remove_array_extent
+    { using type = T_; };
 
-    template <typename T_, std::size_t TSize>
-    struct decay<T_[TSize]>
+    template <typename T_, sizet TSize>
+    struct remove_array_extent<T_[TSize]>
+    { using type = T_; };
+
+    template <typename T_>
+    struct remove_array_extent<T_[]>
+    { using type = T_; };
+
+    template <typename T_>
+    struct array_to_pointer
+    { using type = T_; };
+
+    template <typename T_, sizet TSize>
+    struct array_to_pointer<T_[TSize]>
     { using type = T_*; };
 
-    template <typename TReturnType, typename... TArgs>
-    struct decay<TReturnType(TArgs...)>
-    { using type = TReturnType(*)(TArgs...); };
+    template <typename T_>
+    struct array_to_pointer<T_[]>
+    { using type = T_*; };
+
+    template <typename T_>
+    struct decay
+    {
+        using _1 = remove_ref<T_>;
+        using _2 = select<is_function<T_>>::template _Apply<array_to_pointer<T_>, remove_cv<T_>>;
+        using type = select<is_array<T_>>::template _Apply<array_to_pointer<remove_array_extent<T_>>, _2>::type;
+    };
 
     template <typename T_>
     using decay_t = decay<T_>::type;
-
-    template <typename T_> constexpr bool is_lvalue(T_&)  { return true; }
-    template <typename T_> constexpr bool is_lvalue(T_&&) { return false; }
 
 }
